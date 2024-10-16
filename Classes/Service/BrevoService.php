ef9840c6b0d4bc2555096b6f38d4774a280dd9aa
David Mellen <d.mellen@remind.de>
2024-10-16 15:25:50 +0200

    fix: throw exception if api key is not set

--- Classes/Service/BrevoService.php
+++ Classes/Service/BrevoService.php
@@ -8,6 +8,7 @@ use Brevo\Client\Api\ContactsApi;
 use Brevo\Client\Configuration;
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
+use TYPO3\CMS\Form\Domain\Finishers\Exception\FinisherException;
 
 class BrevoService
 {
@@ -16,6 +17,9 @@ class BrevoService
     public function __construct()
     {
         $apiKey = getenv('BREVO_API_KEY');
+        if (!$apiKey) {
+            throw new FinisherException('Environment variable BREVO_API_KEY is not set', 1729085090);
+        }
         $config = Configuration::getDefaultConfiguration()->setApiKey('api-key', $apiKey);
         $client = new Client([
             RequestOptions::CONNECT_TIMEOUT => 5,
