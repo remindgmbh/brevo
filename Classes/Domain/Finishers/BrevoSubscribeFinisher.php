6c2e9a685c2e7c2bfdd5117b7c9625a2b6cbc851
Dominik Lehrl <d.lehrl@remind.de>
2026-07-13 15:18:21 +0200

    feat: add `errorPage` option
    If set, user will be redirected to given page id in case of an error.

--- Classes/Domain/Finishers/BrevoSubscribeFinisher.php
+++ Classes/Domain/Finishers/BrevoSubscribeFinisher.php
@@ -7,6 +7,7 @@ namespace Remind\Brevo\Domain\Finishers;
 use Brevo\Client\ApiException;
 use Brevo\Client\Model\CreateDoiContact;
 use Brevo\Client\Model\UpdateContact;
+use Throwable;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 
@@ -20,6 +21,7 @@ class BrevoSubscribeFinisher extends AbstractBrevoFinisher
         }
         $templateId = (int) $this->parseOption('templateId');
         $redirectPage = (int) $this->parseOption('redirectPage');
+        $errorPage = (int) ($this->parseOption('errorPage') ?? 0);
         $updateExistingContact = (bool) $this->parseOption('updateExistingContact');
         $formValues = $this->finisherContext->getFormValues();
         $formRuntime = $this->finisherContext->getFormRuntime();
@@ -47,33 +49,46 @@ class BrevoSubscribeFinisher extends AbstractBrevoFinisher
 
         $email = $createDoiContact->getEmail();
 
-        if (
-            $updateExistingContact
-            && $this->contactExists($email)
-        ) {
-            $updateContact = new UpdateContact();
-            $updateContact->setAttributes((object) $attributes);
-            $updateContact->setListIds($listIds);
-            /** @phpstan-ignore-next-line argument.type */
-            $this->contactsApi->updateContact($updateContact, $email, 'email_id');
+        try {
+            if ($this->contactExists($email)) {
+                if ($updateExistingContact) {
+                    $updateContact = new UpdateContact();
+                    $updateContact->setAttributes((object) $attributes);
+                    $updateContact->setListIds($listIds);
+                    /** @phpstan-ignore-next-line argument.type */
+                    $this->contactsApi->updateContact($updateContact, $email, 'email_id');
+
+                    return null;
+                }
+
+                if ($errorPage !== 0) {
+                    return $this->buildRedirectResponse($errorPage);
+                }
+
+                return null;
+            }
+
+            $uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
+            $redirectionUrl = $uriBuilder
+                ->setRequest($formRuntime->getRequest())
+                ->setCreateAbsoluteUri(true)
+                ->setTargetPageUid($redirectPage)
+                ->build();
+
+            $createDoiContact->setAttributes((object) $attributes);
+            $createDoiContact->setIncludeListIds($listIds);
+            $createDoiContact->setTemplateId($templateId);
+            $createDoiContact->setRedirectionUrl($redirectionUrl);
+            $this->contactsApi->createDoiContact($createDoiContact);
+
+            return null;
+        } catch (Throwable) {
+            if ($errorPage !== 0) {
+                return $this->buildRedirectResponse($errorPage);
+            }
 
             return null;
         }
-
-        $uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
-        $redirectionUrl = $uriBuilder
-            ->setRequest($formRuntime->getRequest())
-            ->setCreateAbsoluteUri(true)
-            ->setTargetPageUid($redirectPage)
-            ->build();
-
-        $createDoiContact->setAttributes((object) $attributes);
-        $createDoiContact->setIncludeListIds($listIds);
-        $createDoiContact->setTemplateId($templateId);
-        $createDoiContact->setRedirectionUrl($redirectionUrl);
-        $this->contactsApi->createDoiContact($createDoiContact);
-
-        return null;
     }
 
     protected function contactExists(string $email): bool
@@ -88,5 +103,20 @@ class BrevoSubscribeFinisher extends AbstractBrevoFinisher
             }
             throw $e;
         }
+    }
+
+    protected function buildRedirectResponse(int $targetPage): string
+    {
+        $this->finisherContext->cancel();
+        $redirectUrl = $this->getTypoScriptFrontendController()->cObj->createUrl([
+            'parameter' => $targetPage,
+        ]);
+        return json_encode(
+            [
+                'redirectUrl' => $redirectUrl,
+                'statusCode' => 303,
+            ],
+            JSON_THROW_ON_ERROR
+        );
     }
 }
